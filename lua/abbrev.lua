3004c3c9b5f5a39e3aa5636b651d425970da1a7d
Alex <hellosimon1103@hotmail.com>
2021-10-01 19:07:55 +0800

    Add punctuator/abbrev_suffix option

--- lua/abbrev.lua
+++ lua/abbrev.lua
@@ -1,18 +1,14 @@
-local function filter(input, env)
-  for cand in input:iter() do yield(cand) end
-end
-
 local function segmentor(input, env)
   local context = env.engine.context
   local save_input = context:get_script_text()
-  if (string.sub(save_input, -2) == "::") then
+  local suffix = env.engine.schema.config:get_string("punctuator/abbrev_suffix")
+  if string.sub(save_input, 0 - string.len(suffix)) == suffix then
     context:clear()
-    for i = 1, string.len(save_input) -2 do
+    for i = 1, string.len(save_input) - string.len(suffix) do
       context:push_input(string.sub(save_input, i, i))
       context:push_input("'")
     end
   end
 end
 
-return { filter = filter, segmentor = segmentor }
-
+return segmentor
--- rime.lua
+++ rime.lua
@@ -1,5 +1,4 @@
 date_translator = require("date")
 time_translator = require("time")
-initialism_segmentor = require("initialism").segmentor
-initialism_filter = require("initialism").filter
+abbrev_segmentor = require("abbrev")
 charset_filter = require("charset").filter
