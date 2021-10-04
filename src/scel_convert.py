b6df1c95967409f76cc4f3e3de48224f33debb59
Alex <hellosimon1103@hotmail.com>
2021-10-04 16:18:22 +0800

    Refactor scel_convert.py

--- src/scel_convert.py
+++ src/scel_convert.py
@@ -147,65 +147,22 @@ class scel:
         return self.loads(data)
 
 
-# def getInternetPopularNewWords():
-#     """
-#     从搜狗输入法细胞词库官网下载网络流行新词【官方推荐】
-#     网址是 https://pinyin.sogou.com/dict/detail/index/4
-#     下载地址为 https://pinyin.sogou.com/d/dict/download_cell.php?id=4&name=%E7%BD%91%E7%BB%9C%E6%B5%81%E8%A1%8C%E6%96%B0%E8%AF%8D%E3%80%90%E5%AE%98%E6%96%B9%E6%8E%A8%E8%8D%90%E3%80%91&f=detail
-#     返回： scel文件的二进制 bytes
-#     """
-#     import requests
-
-#     url = "https://pinyin.sogou.com/d/dict/download_cell.php"
-#     params = {
-#         "id": 4,
-#         "name": "网络流行新词【官方推荐】",
-#         "f": "detail",
-#     }
-#     r = requests.get(url, params=params)
-#     return r.content
-
-
-# def main(args):
-#     s = scel()
-
-#     # 读取 scel
-#     if args.file is None:
-#         s.loads(getInternetPopularNewWords())
-#     else:
-#         s.load(args.file)
-
-#     # 生成 text
-#     text = ""
-#     for w in s.word_list:
-#         text += (
-#             w[0]
-#             + "\t"
-#             + " ".join(map(lambda key: s.py_map[key], w[1]))
-#             + "\t"
-#             + str(1)
-#             + "\n"
-#         )
-
-#     # 输出
-#     if args.dest is None:
-#         fp = sys.stdout
-#     else:
-#         fp = open(args.dest + ".dict.yaml", "w")
-#         date = datetime.datetime.now().strftime("%Y-%m-%d")
-#         header = f'---\nname: {args.dest}\nversion: "{date}"\nsort: by_weight\n...\n'
-#         fp.write(header)
-#     fp.write(text)
-
-
 def single_file(args):
     input_path = args.file
     output_path = args.dest
-    # 转换scel为txt
-    GTable = scel2txt(input_path)
-    # 保存结果
-    with open(output_path, "w", encoding="utf8") as f:
-        f.writelines([py + " " + word + "\n" for count, py, word in GTable])
+    try:
+        s = scel()
+        s.load(os.path.join(input_path))
+        text = ""
+        for w in s.word_list:
+            py = " ".join(map(lambda key: s.py_map[key], w[1]))
+            text += w[0] + "\t" + py + "\t" + str(1) + "\n"
+
+        with open(output_path, "w", encoding="utf8") as f:
+            f.write(text)
+            print(input_path + ">>>>>> .dict.yaml 转换成功")
+    except Exception as e:
+        print(e)
 
 
 def batch_file(input_dir, output_dir):
@@ -221,7 +178,6 @@ def batch_file(input_dir, output_dir):
             os.mkdir(new_parent)
         except Exception as e:
             print(e)
-        # 批量处理文件
         for filename in filenames:
             if os.path.exists(
                 os.path.join(new_parent, filename.replace(".scel", ".txt"))
@@ -233,16 +189,9 @@ def batch_file(input_dir, output_dir):
                     s.load(os.path.join(parent, filename))
                     text = ""
                     for w in s.word_list:
-                        text += (
-                            w[0]
-                            + "\t"
-                            + " ".join(map(lambda key: s.py_map[key], w[1]))
-                            + "\t"
-                            + str(1)
-                            + "\n"
-                        )
+                        py = " ".join(map(lambda key: s.py_map[key], w[1]))
+                        text += w[0] + "\t" + py + "\t" + str(1) + "\n"
 
-                    # GTable = scel2txt(os.path.join(parent, filename))
                     with open(
                         os.path.join(
                             new_parent, filename.replace(".scel", ".dict.yaml")
@@ -251,9 +200,6 @@ def batch_file(input_dir, output_dir):
                         encoding="utf8",
                     ) as f:
                         f.write(text)
-                        # f.writelines(
-                        #     [py + " " + word + "\n" for count, py, word in GTable]
-                        # )  # 此处可选择输出的是词频、拼音或是文字
                         print(filename + ">>>>>> .dict.yaml 转换成功")
                 except Exception as e:
                     print(e)
@@ -263,11 +209,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="搜狗细胞词库（.scel）转换工具，" + '输出格式为 "词语\\t拼音\\t优先级"'
     )
-    parser.add_argument("dest", help="输出的 rime 词典名，将生成「词典名.dict.yaml」文件", nargs="?")
     parser.add_argument(
         "file",
         help="搜狗细胞词库文件，格式为 .scel " + "如果不指定则会自动从官网获取“网络流行新词【官方推荐】.scel”",
         nargs="?",
     )
+    parser.add_argument("dest", help="输出的 rime 词典名，将生成「词典名.dict.yaml」文件", nargs="?")
     args = parser.parse_args()
     exit(single_file(args))
