e1c0f2f0406b073572c3b31115f85cf71aa4ee50
Alex <hellosimon1103@hotmail.com>
2021-10-04 16:19:43 +0800

    Remove lua_onlyone_filter

--- rime.lua
+++ rime.lua
@@ -1,7 +1,6 @@
 editor_processor = require("editor")
 date_translator = require("date")
 time_translator = require("time")
-onlyone_filter = require("onlyone")
 abbrev_segmentor = require("abbrev")
 glyph_filter = require("glyph")
 charset_filter = require("charset").filter
