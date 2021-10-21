e47a6d5ed4c57c09f3dd35d6ceadc914004fb346
Alex <hellosimon1103@hotmail.com>
2021-10-22 01:48:20 +0800

    schema: add onechar/charset_filter

--- rime.lua
+++ rime.lua
@@ -1,7 +1,8 @@
+TAB_CARET = 0
 editor_processor = require("editor")
 date_translator = require("date")
 time_translator = require("time")
 abbrev_segmentor = require("abbrev")
 glyph_filter = require("glyph")
-charset_filter = require("charset").filter
-onechar_translator = require("onechar")
+charset_filter = require("charset")
+onechar_filter = require("onechar")
