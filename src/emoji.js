4e2b0335befcf828b61987a4980c9f3be4dff89d
Alex <hellosimon1103@hotmail.com>
2021-10-23 14:14:45 +0800

    dicts: add emoji version in dict header

--- src/emoji.js
+++ src/emoji.js
@@ -1,7 +1,14 @@
 import fs from 'fs'
 import { gemoji } from 'gemoji'
 
-let tmpl = ""
+let tmpl=`---
+name: emoji
+version: "${process.env.GEMOJI_VER}"
+sort: by_weight
+use_preset_vocabulary: true
+...
+`
+
 for (const emoji of gemoji) {
   for (const name of emoji.names) {
     tmpl+=`\n${emoji.emoji}\t${name.replace(/_/g, '')}`
