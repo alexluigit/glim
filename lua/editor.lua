c485f357c2a8ed53ca26a545688e379009c5a5d3
Alex <hellosimon1103@hotmail.com>
2021-10-27 01:47:59 +0800

    lua: add fixed phrase feature

--- lua/editor.lua
+++ lua/editor.lua
@@ -1,3 +1,5 @@
+local input_helper = require("helpers.input")
+
 local function _commit_and_santinize (input, ctx, cand, idx)
   -- todo if input include apostrophe
   if idx then ctx:select(idx) else ctx:confirm_current_selection() end
@@ -11,6 +13,7 @@ local function _commit_and_santinize (input, ctx, cand, idx)
   if not ctx:has_menu() then
     ctx:commit(ctx.get_commit_text(ctx))
     TAB_CARET = 0
+    input_helper.reset(ctx)
   end
 end
 
@@ -26,18 +29,19 @@ local function editor(key, env)
       local cand = ctx:get_selected_candidate()
       _commit_and_santinize(input, ctx, cand)
     elseif key_repr == 'BackSpace' then
+      input_helper.clear(ctx, ctx.caret_pos - TAB_CARET)
       ctx:pop_input(1)
       if not ctx:has_menu() then TAB_CARET = 0 end
       -- todo: reopen commit
     elseif key_repr == 'Return' then
       env.engine:commit_text(input)
       ctx:clear()
+      input_helper.reset(ctx)
       TAB_CARET = 0
     elseif key_repr == 'Escape' then
       ctx:clear()
       TAB_CARET = 0
-    -- elseif key_repr == 'Control+c' then
-    --   print(ctx.caret_pos, TAB_CARET)
+      input_helper.reset(ctx)
     elseif string.match(key_repr, "^%d$") then
       local segment = ctx.composition:back()
       local curr_index = segment.selected_index
@@ -57,7 +61,6 @@ end
 local init = function (env)
   env.page_size = env.engine.schema.config:get_int("menu/page_size")
   -- env.notifier = env.engine.context.update_notifier:connect(function (ctx) end)
-  -- env.engine.context:set_property("tab_caret", 0)
 end
 
 return { init = init, func = editor }
--- lua/glyph.lua
+++ lua/glyph.lua
@@ -2,56 +2,50 @@
 glyph_filter: 候选项重排序, 匹配形码
 --]]
 
-local __cand_hint = function (hint_lvl, py, gl)
-  if hint_lvl == 1 then
-    return "↬ " .. py
-  elseif hint_lvl == 2 then
-    return "↬ " .. py .. " " .. gl
-  end
-end
+local REVERSE_LOOKUP = 1
+local AUTO_GLYPH_WORD = 2
+local AUTO_GLYPH_PHRASE = 3
+local input_helper = require("helpers.input")
+local cand_helper = require("helpers.candidate")
 
-local __filter_char = function (cand, env, ch, glyph_all, keep_phrase, matched)
+local __filter_char = function (cand, env, ch, gl_input, glyph_lvl, matched)
   local py1 = env.glyph_table[ch]["first_py"]
   local py2 = env.glyph_table[ch]["last_py"]
   local gl1 = env.glyph_table[ch]["first_gl"]
   local gl2 = env.glyph_table[ch]["last_gl"]
   local lvl = env.glyph_table[ch]["level"]
   local glyph_filter_str = py1 .. py2
-  local pop_len = (keep_phrase > 1 and glyph_all:len() or glyph_all:len() + 1)
-  if string.match(glyph_filter_str, '^' .. glyph_all) then
-    cand.comment = __cand_hint(env.gl_hint_level, py1 .. py2, gl1 .. gl2)
+  local pop_len = (glyph_lvl > REVERSE_LOOKUP and gl_input:len() or gl_input:len() + 1)
+  if string.match(glyph_filter_str, '^' .. gl_input) then
+    cand.comment = cand_helper.get_hint(env.gl_hint_level, py1 .. py2, gl1 .. gl2)
     cand.type = "glyph_" .. tostring(pop_len)
-    if keep_phrase == 1 or lvl < 3 then
+    if glyph_lvl == REVERSE_LOOKUP or lvl < 3 then
       table.insert(matched, cand)
     end
   end
 end
 
 local __filter_phrase = function
-    (cand, env, tail_ch, head_ch, glyph_all, keep_phrase, ph_top, matched, rest)
+    (cand, env, tail_ch, head_ch, gl_input, glyph_lvl, ph_top, matched, rest)
   local t_py1 = env.glyph_table[tail_ch]["first_py"]
-  local t_py2 = env.glyph_table[tail_ch]["last_py"]
   local t_gl1 = env.glyph_table[tail_ch]["first_gl"]
-  local t_gl2 = env.glyph_table[tail_ch]["last_gl"]
   local t_lvl = env.glyph_table[head_ch]["level"]
   local h_py1 = env.glyph_table[head_ch]["first_py"]
-  local h_py2 = env.glyph_table[head_ch]["last_py"]
   local h_gl1 = env.glyph_table[head_ch]["first_gl"]
-  local h_gl2 = env.glyph_table[head_ch]["last_gl"]
   local h_lvl = env.glyph_table[head_ch]["level"]
-  local glyph_filter_str = h_py1 .. t_py1 .. t_py2 .. h_py2
-  local glyph_gl_str = h_gl1 .. t_gl1 .. t_gl2 .. h_gl2
+  local glyph_filter_str = h_py1 .. t_py1
+  local glyph_gl_str = h_gl1 .. t_gl1
   local text = cand.text
-  if ph_top == nil and utf8.len(text) == keep_phrase then ph_top = cand end
-  if keep_phrase == 1 and string.match(glyph_filter_str, '^' .. glyph_all) then
-    cand.comment = __cand_hint(env.gl_hint_level, glyph_filter_str, glyph_gl_str)
-    cand.type = "glyph_" .. tostring(glyph_all:len() + 1)
+  if ph_top == nil and utf8.len(text) == glyph_lvl then ph_top = cand end
+  if glyph_lvl == REVERSE_LOOKUP and string.match(glyph_filter_str, '^' .. gl_input) then
+    cand.comment = cand_helper.get_hint(env.gl_hint_level, glyph_filter_str, glyph_gl_str)
+    cand.type = "glyph_" .. tostring(gl_input:len() + 1)
     yield(cand)
-  elseif utf8.len(text) < keep_phrase
-    and string.match(glyph_filter_str, '^' .. glyph_all)
+  elseif utf8.len(text) < glyph_lvl
+    and string.match(glyph_filter_str, '^' .. gl_input)
     and t_lvl < 3 and h_lvl < 3 then
-    cand.comment = __cand_hint(env.gl_hint_level, h_py1 .. t_py1, h_gl1 .. t_gl1)
-    cand.type = "glyph_" .. tostring(glyph_all:len())
+    cand.comment = cand_helper.get_hint(env.gl_hint_level, glyph_filter_str, glyph_gl_str)
+    cand.type = "glyph_" .. tostring(gl_input:len())
     table.insert(matched, cand)
   elseif cand.text ~= (ph_top and ph_top.text) then
     table.insert(rest, cand)
@@ -59,67 +53,67 @@ local __filter_phrase = function
   return ph_top
 end
 
-local __get_words = function (cand, env)
-  local text = cand.text
-  local ch_idxs = {}
-  for i in utf8.codes(text) do table.insert(ch_idxs, i) end
-  local tail_ch = utf8.char(utf8.codepoint(text, ch_idxs[#ch_idxs]))
-  local head_ch = nil
-  if #ch_idxs > 1 then head_ch = utf8.char(utf8.codepoint(text, ch_idxs[#ch_idxs - 1])) end
-  local valid_tail_word = env.glyph_table[tail_ch]
-  local valid_head_word = (not head_ch) or env.glyph_table[head_ch]
-  return tail_ch, head_ch, (valid_tail_word and valid_head_word)
-end
-
-local __sort_matches = function (env, ph_top, glyph_all, matched, rest, keep_phrase)
-  if ph_top then
-    if glyph_all:len() == 1 then
-      for i, cand in ipairs(matched) do yield(cand) end
-      yield(ph_top)
-    else
-      local ontop = ph_top.quality >= env.gl_min_freq
-      -- print("freqe: ", string.format("%.5f", env.gl_min_freq))
-      -- print("first: ", string.format("%.5f", ph_top.quality), ph_top.text)
-      if ontop then
-        yield(ph_top)
-        for i, cand in ipairs(matched) do yield(cand) end
-      else
-        for i, cand in ipairs(matched) do yield(cand) end
-        yield(ph_top)
-      end
-    end
-  else
+local __display_matches = function (env, ph_top, gl_input, matched, rest, glyph_lvl)
+  cand_helper.sort_by_rank(env.charset_table, matched, glyph_lvl)
+  if glyph_lvl == REVERSE_LOOKUP then
     for i, cand in ipairs(matched) do yield(cand) end
+    return
   end
-  if keep_phrase ~= 1 then
-    for i, cand in ipairs(rest) do yield(cand) end
+  local ctx = env.engine.context
+  local caret = gl_input:len() + 2 * (glyph_lvl - 1)
+  local recorded = false
+  local history = input_helper.get_history(ctx, caret - 1)
+  local valid_match = {}
+  local ph_text = ph_top and ph_top.text
+  local top_qual = 0
+  local rule = env.gl_ranking_rule
+  local TOP, BOTTOM = 1, 2
+  for i, cand in ipairs(matched) do
+    if input_helper.validate(history, cand.text) then
+      top_qual = math.max(top_qual, cand.quality)
+      table.insert(valid_match, cand)
+    end
   end
+  local place_phrase = cand_helper.place_phrase(
+    ph_top, valid_match, rule, top_qual, caret)
+  if place_phrase == TOP then
+    yield(ph_top)
+    recorded = input_helper.set_history(ctx, caret, ph_text)
+  end
+  for i, cand in ipairs(valid_match) do
+    if not recorded then
+      recorded = input_helper.set_history(ctx, caret, cand.text)
+    end
+    yield(cand)
+  end
+  if place_phrase == BOTTOM then yield(ph_top) end
+  for i, cand in ipairs(rest) do yield(cand) end
 end
 
-local _filter_cands = function (cands, env, glyph_all, keep_phrase)
+local _filter_cands = function (cands, env, gl_input, glyph_lvl)
   local ph_top = nil
   local matched = {}
   local rest = {}
   for cand in cands:iter() do
-    local tail_ch, head_ch, valid_words = __get_words(cand, env)
+    local tail_ch, head_ch, valid_words = cand_helper.get_words(cand, env.glyph_table)
     if not valid_words then table.insert(rest, cand); goto skip_to_next end
     if head_ch then
       ph_top = __filter_phrase(
-        cand, env, tail_ch, head_ch, glyph_all, keep_phrase, ph_top, matched, rest)
-    elseif keep_phrase <= 2 then
-      __filter_char(cand, env, tail_ch, glyph_all, keep_phrase, matched)
+        cand, env, tail_ch, head_ch, gl_input, glyph_lvl, ph_top, matched, rest)
+    elseif glyph_lvl <= 2 then
+      __filter_char(cand, env, tail_ch, gl_input, glyph_lvl, matched)
     else
       table.insert(rest, cand)
     end
     ::skip_to_next::
   end
-  __sort_matches(env, ph_top, glyph_all, matched, rest, keep_phrase)
+  __display_matches(env, ph_top, gl_input, matched, rest, glyph_lvl)
 end
 
 local _match_input = function (caret, input, auto_lvl, lvl)
-  if auto_lvl > lvl - 1 and string.match(input, '^[a-z ;]+$')
-    and caret - TAB_CARET > 2 * lvl
-    and caret - TAB_CARET <= 2 * (1 + lvl) then
+  local seg_len = caret - TAB_CARET
+  if auto_lvl > lvl - 1 and input:match('^[a-z ;]+$')
+    and seg_len > 2 * lvl and seg_len <= 2 * (1 + lvl) then
     return true
   end
 end
@@ -129,14 +123,14 @@ local filter = function (cands, env)
   local input = ctx.input
   local caret = ctx.caret_pos
   local auto_lvl = env.gl_auto_level
-  local glyph_all = input:sub(caret - (caret % 2 == 1 and 0 or 1), caret)
+  local gl_input = input:sub(caret - (caret % 2 == 1 and 0 or 1), caret)
   if string.match(input, ':%a?') then
-    glyph_all = string.gsub(input, ".*:", "")
-    _filter_cands(cands, env, glyph_all, 1)
+    gl_input = string.gsub(input, ".*:", "")
+    _filter_cands(cands, env, gl_input, REVERSE_LOOKUP)
   elseif _match_input(caret, input, auto_lvl, 1) then
-    _filter_cands(cands, env, glyph_all, 2)
+    _filter_cands(cands, env, gl_input, AUTO_GLYPH_WORD)
   elseif _match_input(caret, input, auto_lvl, 2) then
-    _filter_cands(cands, env, glyph_all, 3)
+    _filter_cands(cands, env, gl_input, AUTO_GLYPH_PHRASE)
   else
     for cand in cands:iter() do yield(cand) end
   end
@@ -154,13 +148,12 @@ local init = function (env)
     ["double_plus"]=2,
     ["chole"]=2
   }
-  local freq_lvl = { 0, 1, 3, 5, 10, 50, 100, 1000, 5000, 10000 }
-  local min_freq = freq_lvl[ config:get_int("translator/glyph_min_freq") + 1 ] or 0
   env.gl_hint_level = config:get_int("translator/glyph_hint_level")
   env.gl_auto_level = config:get_int("translator/glyph_auto_level")
-  env.gl_min_freq = min_freq * 0.51 / 10000
+  env.gl_ranking_rule = config:get_string("translator/glyph_ranking_rule")
   if layout == "full" then env.gl_auto_level = 0 end
   env.glyph_table = (layout_types[layout] == 2 and glyph.table_II or glyph.table_I)
+  env.charset_table = require("charset_table")
 end
 
 return { init = init, func = filter }
