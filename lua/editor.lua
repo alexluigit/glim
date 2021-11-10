9dd139936d2bd3fb11a27f8a58bbf486d6b2a704
Alex <hellosimon1103@hotmail.com>
2021-11-10 17:14:14 +0800

    lua: add 'navigate_with' option

--- lua/editor.lua
+++ lua/editor.lua
@@ -1,4 +1,5 @@
 local input_helper = require("helpers.input")
+local key_helper = require("helpers.key_event")
 
 local function _commit_and_santinize (env, cand, idx)
   local ctx = env.engine.context
@@ -12,55 +13,60 @@ local function _commit_and_santinize (env, cand, idx)
   ctx.caret_pos = ctx.input:len()
   if not ctx:has_menu() then
     -- BUG? If context is 'composing' here, user dict do not pick up new word.
-    -- print(ctx:is_composing())
     ctx:commit()
     TAB_CARET = 0
     input_helper.reset(ctx)
   end
 end
 
-local function editor(key, env)
+local function editor(key_event, env)
   local kAccepted = 1
   local kNoop = 2
-  local key_repr = key:repr() -- key representation
   local ctx = env.engine.context
-  local input = ctx.input
+  local key_press = key_helper:init{ kCode = key_event.keycode }
   -- local tab_caret = ctx:get_property("TAB_CARET")
-  if ctx:has_menu() and string.len(input) > 0 then
-    if key_repr == 'space' then
-      local cand = ctx:get_selected_candidate()
-      _commit_and_santinize(env, cand)
-    elseif key_repr == 'BackSpace' then
-      input_helper.clear(ctx, ctx.caret_pos - TAB_CARET)
-      ctx:pop_input(1)
-      if not ctx:has_menu() then TAB_CARET = 0 end
-    elseif key_repr == 'Return' then
-      env.engine:commit_text(input)
-      ctx:clear()
-      input_helper.reset(ctx)
-      TAB_CARET = 0
-    elseif key_repr == 'Escape' then
-      ctx:clear()
-      TAB_CARET = 0
-      input_helper.reset(ctx)
-    elseif string.match(key_repr, "^%d$") then
-      local segment = ctx.composition:back()
-      local curr_index = segment.selected_index
-      local page_start_index = curr_index - curr_index % env.page_size
-      local computed_index = page_start_index + tonumber(key_repr) - 1
-      local cand = segment:get_candidate_at(computed_index)
-      _commit_and_santinize(env, cand, computed_index)
-    else
-      return kNoop
-    end
-    return kAccepted
+  local should_forward_key = (not ctx:has_menu())
+    or key_event:release() or key_event:ctrl() or key_event:alt()
+  if should_forward_key then return kNoop end
+  local key_repr = key_event:repr() -- key representation
+  local input = ctx.input
+  if key_press:is_space() then
+    local cand = ctx:get_selected_candidate()
+    _commit_and_santinize(env, cand)
+  elseif key_press:is_backspace() then
+    input_helper.clear(ctx, ctx.caret_pos - TAB_CARET)
+    ctx:pop_input(1)
+    if not ctx:has_menu() then TAB_CARET = 0 end
+  elseif key_press:is_return() then
+    env.engine:commit_text(input)
+    ctx:clear()
+    input_helper.reset(ctx)
+    TAB_CARET = 0
+  elseif key_press:is_escape() then
+    ctx:clear()
+    TAB_CARET = 0
+    input_helper.reset(ctx)
+  elseif key_press:is_digit() then
+    local segment = ctx.composition:back()
+    local curr_index = segment.selected_index
+    local page_start_index = curr_index - curr_index % env.page_size
+    local computed_index = page_start_index + tonumber(key_repr) - 1
+    local cand = segment:get_candidate_at(computed_index)
+    _commit_and_santinize(env, cand, computed_index)
+  elseif env.navi_index and key_press:is_upper() then
+    ctx.caret_pos = TAB_CARET + env.navi_index[key_repr]
   else
     return kNoop
   end
+  return kAccepted
 end
 
 local init = function (env)
-  env.page_size = env.engine.schema.config:get_int("menu/page_size")
+  local config = env.engine.schema.config
+  local layout = config:get_string("speller/layout")
+  env.navi_index = key_helper.to_navi_index(config:get_string("glim/navigate_with"))
+  if layout == "full" then env.navi_index = false end
+  env.page_size = config:get_int("menu/page_size")
 end
 
 return { init = init, func = editor }
---
+++ lua/helpers/key_event.lua
@@ -0,0 +1,48 @@
+Key = { kCode = 0x00 }
+
+function Key:init (o)
+  o = o or {}
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+function Key.to_navi_index(str)
+  if str == "" then return false end
+  local t = {}
+  for i = 1, #str do
+    local c = str:sub(i,i):upper()
+    t["Shift+" .. c] = i * 2
+  end
+  return t
+end
+
+function Key:is_space ()
+  return self.kCode == 0x20
+end
+
+function Key:is_backspace ()
+  return self.kCode == 0xff08
+end
+
+function Key:is_return ()
+  return self.kCode == 0xff0d
+end
+
+function Key:is_escape ()
+  return self.kCode == 0xff1b
+end
+
+function Key:is_digit ()
+  return self.kCode > 0x30 and self.kCode <= 0x39
+end
+
+function Key:is_upper ()
+  return self.kCode > 0x40 and self.kCode <= 0x5a
+end
+
+function Key:is_lower ()
+  return self.kCode > 0x60 and self.kCode <= 0x7a
+end
+
+return Key
